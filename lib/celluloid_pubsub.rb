6e1df932ca45c25d7e756df8a03b12d1f338130d
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-08-28 18:58:45 +0300

    updates

--- lib/celluloid_pubsub.rb
+++ lib/celluloid_pubsub.rb
@@ -3,6 +3,5 @@ require 'celluloid/io'
 require 'reel'
 require 'celluloid/websocket/client'
 require 'active_support/all'
-require 'celluloid_pubsub/config'
-CelluloidPubsub::Config.backward_compatible
+require 'celluloid_pubsub/backward_compatible'
 Gem.find_files('celluloid_pubsub/**/*.rb').each { |path| require path }
--- lib/celluloid_pubsub/client_pubsub.rb
+++ lib/celluloid_pubsub/client_pubsub.rb
@@ -27,7 +27,7 @@ module CelluloidPubsub
     #   @return [String] The hostname on which the webserver runs on
     class PubSubWorker
       include Celluloid
-      include CelluloidPubsub::Config.config['logger_class']
+      include CelluloidPubsub::BackwardCompatible.config['logger_class']
       attr_accessor :actor, :client, :options, :hostname, :port, :path, :channel
 
       #  receives a list of options that are used to connect to the webserver and an actor to which the callbacks are delegated to
