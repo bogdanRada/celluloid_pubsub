6f88011d1cc3501c33a2d7558f5b10b8fb983a32
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-11-25 12:25:06 +0200

    updatE

--- lib/celluloid_pubsub.rb
+++ lib/celluloid_pubsub.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'bundler'
 require 'versionomy'
-require 'celluloid_pubsub/backward_compatible'
 require 'celluloid/io'
 require 'reel'
 require 'celluloid/websocket/client'
 require 'active_support/all'
+require 'celluloid_pubsub/backward_compatible'
 Gem.find_files('celluloid_pubsub/**/*.rb').each { |path| require path }
