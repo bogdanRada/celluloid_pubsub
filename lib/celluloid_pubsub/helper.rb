07836299eb27f62d9b1c422ef1a71b39c8545134
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-12-21 10:50:30 +0200

    update

--- lib/celluloid_pubsub/helper.rb
+++ lib/celluloid_pubsub/helper.rb
@@ -9,22 +9,23 @@ module Helper
     gem_spec.respond_to?(property) ? gem_spec.send(property) : nil
   end
 
-  def fetch_gem_version(gem_name, options = {})
+  def fetch_gem_version(gem_name)
     version = find_loaded_gem_property(gem_name, 'version')
-    version.blank? ? nil : get_parsed_version(version.to_s, options)
+    version.blank? ? nil : get_parsed_version(version)
   end
 
-  def get_parsed_version(version, options)
-    parsing_options = {optional_fields: [:tiny]}.merge(options.fetch("unparse", {}))
-    Versionomy.parse(version).unparse(parsing_options)
+  def get_parsed_version(version)
+    version = version.to_s.split('.')
+    version.pop until version.size == 2
+    Versionomy.parse(version.join('.'))
   rescue Versionomy::Errors::ParseError
     nil
   end
 
   def verify_gem_version(gem_name, version, options = {})
     options.stringify_keys!
-    version = get_parsed_version(version, options)
-    gem_version = fetch_gem_version(gem_name, options)
+    version = get_parsed_version(version)
+    gem_version = fetch_gem_version(gem_name)
     gem_version.blank? ? false : gem_version.send(options.fetch('operator', '<='), version)
   end
 
