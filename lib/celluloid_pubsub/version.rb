3f73ac42cbf561b1e351c427a30d82aebaf8dc5f
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-07-08 14:38:07 +0300

    update

--- lib/celluloid_pubsub/version.rb
+++ lib/celluloid_pubsub/version.rb
@@ -17,7 +17,7 @@ module CelluloidPubsub
     # minor release version
     MINOR = 8
     # tiny release version
-    TINY = 1
+    TINY = 2
     # prelease version ( set this only if it is a prelease)
     PRE = nil
 
