f95568a499a6caa6459f3451ee85b37bb75a1e63
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-09-26 11:06:53 +0300

    fix tests and issue with unsubscribe clients

--- lib/celluloid_pubsub/version.rb
+++ lib/celluloid_pubsub/version.rb
@@ -19,10 +19,10 @@ module CelluloidPubsub
     # minor release version
     MINOR = 0
     # tiny release version
-    TINY = 0
+    TINY = 1
     # prelease version ( set this only if it is a prelease)
     PRE = nil
-    
+
     # generates the version string
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
