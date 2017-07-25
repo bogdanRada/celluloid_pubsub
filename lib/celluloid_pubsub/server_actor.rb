32ffcaa09cb24ed0d7b6e1f87ecc910ab7ba8e22
Rada Bogdan Raul <raoul_ice@yahoo.com>
2017-07-25 15:36:17 +0300

    make it work with HTTP also  :D

--- lib/celluloid_pubsub/server_actor.rb
+++ lib/celluloid_pubsub/server_actor.rb
@@ -258,6 +258,9 @@ module CelluloidPubsub
       route_websocket(reactor, request.websocket)
     end
 
+    # Compile the regex once
+    CONTENT_LENGTH_HEADER = %r{^content-length$}i
+
     #  HTTP connections are not accepted so this method will show 404 message "Not Found"
     #
     # @param [Reel::WebSocket] connection The HTTP connection that was received
@@ -266,9 +269,80 @@ module CelluloidPubsub
     # @return [void]
     #
     # @api public
-    def route_request(connection, request)
-      log_debug "404 Not Found: #{request.path}"
-      connection.respond :not_found, 'Not found'
+    def route_request(request)
+      options = {
+        :method       => request.method,
+        :input        => request.body.to_s,
+        "REMOTE_ADDR" => request.remote_addr
+      }.merge(convert_headers(request.headers))
+
+      normalize_env(options)
+
+      status, headers, body = app.call ::Rack::MockRequest.env_for(request.url, options)
+
+      if body.respond_to? :each
+        # If Content-Length was specified we can send the response all at once
+        if headers.keys.detect { |h| h =~ CONTENT_LENGTH_HEADER }
+          # Can't use collect here because Rack::BodyProxy/Rack::Lint isn't a real Enumerable
+          full_body = ''
+          body.each { |b| full_body << b }
+          request.respond status_symbol(status), headers, full_body
+        else
+          request.respond status_symbol(status), headers.merge(:transfer_encoding => :chunked)
+          body.each { |chunk| request << chunk }
+          request.finish_response
+        end
+      else
+        Logger.error("don't know how to render: #{body.inspect}")
+        request.respond :internal_server_error, "An error occurred processing your request"
+      end
+
+      body.close if body.respond_to? :close
+    end
+
+    # Those headers must not start with 'HTTP_'.
+    NO_PREFIX_HEADERS=%w[CONTENT_TYPE CONTENT_LENGTH].freeze
+
+    def convert_headers(headers)
+      Hash[
+        headers.map do |key, value|
+          header = key.upcase.gsub('-','_')
+
+          if NO_PREFIX_HEADERS.member?(header)
+            [header, value]
+          else
+            ['HTTP_' + header, value]
+          end
+        end
+      ]
+    end
+
+    # Copied from lib/puma/server.rb
+    def normalize_env(env)
+      if host = env["HTTP_HOST"]
+        if colon = host.index(":")
+          env["SERVER_NAME"] = host[0, colon]
+          env["SERVER_PORT"] = host[colon+1, host.bytesize]
+        else
+          env["SERVER_NAME"] = host
+          env["SERVER_PORT"] = default_server_port(env)
+        end
+      else
+        env["SERVER_NAME"] = "localhost"
+        env["SERVER_PORT"] = default_server_port(env)
+      end
+    end
+
+    def default_server_port(env)
+      env['HTTP_X_FORWARDED_PROTO'] == 'https' ? 443 : 80
+    end
+
+    def status_symbol(status)
+      if status.is_a?(Fixnum)
+        Reel::Response::STATUS_CODES[status].downcase.gsub(/\s|-/, '_').to_sym
+      else
+        status.to_sym
+      end
     end
 
     #  If the socket url matches with the one accepted by the server, it will dispatch the socket connection to a new reactor {CelluloidPubsub::Reactor#work}
